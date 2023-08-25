cf5595fe1b7f6d7c6a0b404f707a6d7c4e8ab0cb
jjh <slam7707@gmail.com>
2023-08-25 16:28:11 +0900

    .

--- app/src/main/java/com/jh/nmc1/NMC1View.kt
+++ app/src/main/java/com/jh/nmc1/NMC1View.kt
@@ -18,13 +18,14 @@ class NMC1View (context : Context) : View(context) {
         this.m_context = context
 
         val display = m_context.applicationContext?.resources?.displayMetrics
-        val deviceCX = display?.widthPixels
-        val deviceCY = display?.heightPixels
+//      val deviceCX = display?.widthPixels
+//      val deviceCY = display?.heightPixels
 
-        m_rect = Rect(0,0,deviceCX!!.toInt(), deviceCY!!.toInt())
+        m_rect = Rect(0,0, display?.widthPixels!!.toInt(), display?.heightPixels!!.toInt())
 
-        fun px2dp(px: Int, context: Context) = px / ((context.resources.displayMetrics.densityDpi.toFloat()) / DisplayMetrics.DENSITY_DEFAULT)
-        Log.d("deviceSize","cx = ${deviceCX}, cy = ${deviceCY}, ${px2dp(deviceCX!!, m_context)}, ${px2dp(deviceCY!!, m_context)}")
+//        fun px2dp(px: Int, context: Context) = px / ((context.resources.displayMetrics.densityDpi.toFloat()) / DisplayMetrics.DENSITY_DEFAULT)
+//        Log.d("deviceSize","cx = ${deviceCX}, cy = ${deviceCY}, ${px2dp(deviceCX!!, m_context)}, ${px2dp(deviceCY!!, m_context)}")
+        Log.d("deviceSize","cx = ${display?.widthPixels!!.toInt()}, cy = ${display?.heightPixels!!.toInt()}")
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -36,7 +37,7 @@ class NMC1View (context : Context) : View(context) {
 
         val green = Paint()
         green.style = Paint.Style.STROKE
-        green.strokeWidth = 1f
+        green.strokeWidth = 10f
         green.color =Color.GREEN
         canvas?.drawRect(m_rect, green)
     }
